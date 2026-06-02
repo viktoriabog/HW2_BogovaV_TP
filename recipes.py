d1d9389060740104319c87f7064d5661ff28159a
Victoria Bogova <viktoria.bog007g@gmail.com>
2026-06-02 18:40:21 +0300

    класc DietaryRecipe

--- recipes.py
+++ recipes.py
@@ -151,3 +151,26 @@ class ShoppingList:
             self._items[:] +
             other._items[:])
         return merged
+    
+
+class DietaryRecipe(Recipe):
+
+    def __init__(self,title: str,
+        diet_type: str,
+        ingredients=None):
+        super().__init__(title,ingredients)
+        self.diet_type = diet_type
+
+    def scale(self, ratio: float):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(
+            scaled_recipe.title,
+            self.diet_type,
+            scaled_recipe.ingredients
+        )
+
+    def __str__(self):
+        return (
+            f"[{self.diet_type}] "
+            f"{super().__str__()}"
+        )
